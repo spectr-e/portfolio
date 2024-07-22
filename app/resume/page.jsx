ada3c89683102a30ca410d248231953b49a2052f
spectr-e <devs.josia@gmail.com>
2024-07-22 15:48:02 +0300

    fix(Education): Add my education background

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -1,4 +1,4 @@
-import { Experiences } from '@/components'
+import { Education, Experiences } from '@/components'
 
 const ResumePage = () => {
   return (
@@ -11,54 +11,7 @@ const ResumePage = () => {
       <Experiences />
 
       {/* education */}
-      <section className='timeline'>
-        <div className='title-wrapper'>
-          <div className='icon-box'>
-            <ion-icon name='book-outline'></ion-icon>
-          </div>
-
-          <h3 className='h3'>Education</h3>
-        </div>
-
-        <ol className='timeline-list'>
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>
-              University school of the arts
-            </h4>
-
-            <span>2007 — 2008</span>
-
-            <p className='timeline-text'>
-              Nemo enims ipsam voluptatem, blanditiis praesentium voluptum
-              delenit atque corrupti, quos dolores et quas molestias exceptur.
-            </p>
-          </li>
-
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>New york academy of art</h4>
-
-            <span>2006 — 2007</span>
-
-            <p className='timeline-text'>
-              Ratione voluptatem sequi nesciunt, facere quisquams facere menda
-              ossimus, omnis voluptas assumenda est omnis..
-            </p>
-          </li>
-
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>
-              High school of art and design
-            </h4>
-
-            <span>2002 — 2004</span>
-
-            <p className='timeline-text'>
-              Duis aute irure dolor in reprehenderit in voluptate, quila
-              voluptas mag odit aut fugit, sed consequuntur magni dolores eos.
-            </p>
-          </li>
-        </ol>
-      </section>
+      <Education />
 
       {/* skills */}
       <section className='skill'>
--- components/Education.jsx
+++ components/Education.jsx
@@ -1,5 +1,43 @@
 const Education = () => {
-  return <div></div>
+  return (
+    <section className='timeline'>
+      <div className='title-wrapper'>
+        <div className='icon-box'>
+          <ion-icon name='book-outline'></ion-icon>
+        </div>
+
+        <h3 className='h3'>Education</h3>
+      </div>
+
+      <ol className='timeline-list'>
+        <li className='timeline-item'>
+          <h4 className='h4 timeline-item-title'>
+            Bachelor of Commerce | University of Nairobi
+          </h4>
+
+          <span className='text-sm mb-2'>2020 — 2024</span>
+
+          <p className='timeline-text'>
+            Emboldened in several disciplines including project management,
+            business operations and product design.
+          </p>
+        </li>
+
+        <li className='timeline-item'>
+          <h4 className='h4 timeline-item-title'>
+            Certificate in Software Engineering | Moringa School
+          </h4>
+
+          <span className='text-sm mb-2'>2022 - 2023</span>
+
+          <p className='timeline-text'>
+            Partaken courses in frontend web development with React.js and
+            backend web development with Ruby on Rails.
+          </p>
+        </li>
+      </ol>
+    </section>
+  )
 }
 
 export default Education
