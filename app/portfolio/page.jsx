d93844b73650ee9299c22e263274ccd3d84ea02a
spectr-e <devs.josia@gmail.com>
2024-07-22 10:50:59 +0300

    feat(portfolio): Add the section structure

---
+++ app/portfolio/page.jsx
@@ -0,0 +1,322 @@
+import {
+  project1,
+  project2,
+  project3,
+  project4,
+  project5,
+  project6,
+  project7,
+  project8,
+  project9,
+} from '@/assets/styles/images'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const PortfolioPage = () => {
+  return (
+    <article className='portfolio active' data-page='portfolio'>
+      <header>
+        <h2 className='h2 article-title'>Portfolio</h2>
+      </header>
+
+      <section className='projects'>
+        <ul className='filter-list'>
+          <li className='filter-item'>
+            <button className='active' data-filter-btn>
+              All
+            </button>
+          </li>
+
+          <li className='filter-item'>
+            <button data-filter-btn>Web design</button>
+          </li>
+
+          <li className='filter-item'>
+            <button data-filter-btn>Applications</button>
+          </li>
+
+          <li className='filter-item'>
+            <button data-filter-btn>Web development</button>
+          </li>
+        </ul>
+
+        <div className='filter-select-box'>
+          <button className='filter-select' data-select>
+            <div className='select-value' data-selecct-value>
+              Select category
+            </div>
+
+            <div className='select-icon'>
+              <ion-icon name='chevron-down'></ion-icon>
+            </div>
+          </button>
+
+          <ul className='select-list'>
+            <li className='select-item'>
+              <button data-select-item>All</button>
+            </li>
+
+            <li className='select-item'>
+              <button data-select-item>Web design</button>
+            </li>
+
+            <li className='select-item'>
+              <button data-select-item>Applications</button>
+            </li>
+
+            <li className='select-item'>
+              <button data-select-item>Web development</button>
+            </li>
+          </ul>
+        </div>
+
+        <ul className='project-list'>
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web development'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project1}
+                  alt='finance'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Finance</h3>
+
+              <p className='project-category'>Web development</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web development'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project2}
+                  alt='orizon'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Orizon</h3>
+
+              <p className='project-category'>Web development</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web design'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project3}
+                  alt='fundo'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Fundo</h3>
+
+              <p className='project-category'>Web design</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='applications'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project4}
+                  alt='brawlhalla'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Brawlhalla</h3>
+
+              <p className='project-category'>Applications</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web design'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project5}
+                  alt='dsm.'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>DSM.</h3>
+
+              <p className='project-category'>Web design</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web design'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project6}
+                  alt='metaspark'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>MetaSpark</h3>
+
+              <p className='project-category'>Web design</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web development'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project7}
+                  alt='summary'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Summary</h3>
+
+              <p className='project-category'>Web development</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='applications'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project8}
+                  alt='task manager'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Task Manager</h3>
+
+              <p className='project-category'>Applications</p>
+            </Link>
+          </li>
+
+          <li
+            className='project-item active'
+            data-filter-item
+            data-category='web development'
+          >
+            <Link href='#'>
+              <figure className='project-img'>
+                <div className='project-item-icon-box'>
+                  <ion-icon name='eye-outline'></ion-icon>
+                </div>
+
+                <Image
+                  sizes='100vh'
+                  width={0}
+                  height={0}
+                  src={project9}
+                  alt='arrival'
+                  loading='lazy'
+                />
+              </figure>
+
+              <h3 className='project-title'>Arrival</h3>
+
+              <p className='project-category'>Web development</p>
+            </Link>
+          </li>
+        </ul>
+      </section>
+    </article>
+  )
+}
+
+export default PortfolioPage
