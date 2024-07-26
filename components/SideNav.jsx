6bd96eb1d6c102a6282766ad9442c4686d00062d
spectr-e <devs.josia@gmail.com>
2024-07-26 03:38:47 +0300

    fix(SideNav): Change address url

--- components/SideNav.jsx
+++ components/SideNav.jsx
@@ -164,9 +164,7 @@ const SideNav = () => {
 
               <Link
                 target='_blank'
-                href={
-                  'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255282.32335424496!2d36.84739685!3d-1.3032089500000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f1172d84d49a7%3A0xf7cf0254b297924c!2sNairobi!5e0!3m2!1sen!2ske!4v1721638069036!5m2!1sen!2ske'
-                }
+                href={'https://maps.app.goo.gl/e7HQAjzXevSY7AoY9'}
                 className='contact-link'
               >
                 Nairobi, Kenya
